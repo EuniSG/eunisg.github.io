ce2b9ccc98db190a52949e165dc353290f8dd347
Eunice Santos <eunisantos218@gmail.com>
2023-03-20 18:20:55 -0600

    Color update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { LeadershipComponent } from './leadership/leadership.component';
+import { AwardsComponent } from './awards/awards.component';
+import { SkillsSoftComponent } from './skills-soft/skills-soft.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WorkExperienceComponent
+    WorkExperienceComponent,
+    SkillsComponent,
+    LeadershipComponent,
+    AwardsComponent,
+    SkillsSoftComponent
   ],
   imports: [
     BrowserModule,
